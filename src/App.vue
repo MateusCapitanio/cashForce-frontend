cfe7bd30350ad1b6fc64766de308f8f5b3499427
Mateus Capitanio <mateusjmj92@gmail.com>
2022-12-30 11:49:12 -0300

    Criando components Header e Button

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
 <script setup>
-import Button from "./components/ButtonComp.vue";
+// import Button from "./components/ButtonComp.vue";
 import getRequest from "./api/baseRequest";
+import Header from "./components/HeaderComp.vue";
 
 getRequest("");
 </script>
 
 <template>
   <main>
-    <aside>Aside</aside>
-    <Button />
+    <aside>
+      <img class="classImg" src="./assets/logo.jpeg" />
+      <div class="selectionDiv">
+        <p>Notas fiscais</p>
+      </div>
+    </aside>
+    <section class="divSection">
+      <Header />
+    </section>
   </main>
 </template>
 
 <style scoped>
-h1 {
-  background: red;
-}
-
 main {
   color: black;
   display: flex;
@@ -24,6 +28,33 @@ main {
 
 aside {
   width: 20%;
-  background: green;
+  height: 100vh;
+  background: #f7f7f7;
+}
+
+.classImg {
+  width: 250px;
+  margin: 10px;
+}
+
+.selectionDiv {
+  border-left: 5px solid #00be8a;
+  margin: 12px 0;
+  padding: 20px 0;
+  display: flex;
+  justify-content: center;
+}
+
+.selectionDiv p {
+  color: #00be8a;
+  font-weight: 500;
+  font-size: 18px;
+}
+
+.divSection {
+  background: #f7f7f7;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px 0px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px 0px;
+  width: 100%;
 }
 </style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,18 @@
+<script>
+// export default {
+//   props: { onClickBtn: Function },
+// };
+</script>
+<template>
+  <header class="divHeader">Olá, sou um header!</header>
+</template>
+
+<style scoped>
+.divHeader {
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  background-color: #f7f7f7;
+  padding: 20px 0;
+  width: 100%;
+}
+</style>
